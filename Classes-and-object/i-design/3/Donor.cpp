4cb9e46fb16dcab98f796727c2f957eb40011ec2
hardik2901 <72349855+hardik2901@users.noreply.github.com>
2021-03-13 01:21:40 +0530

    Create Donor.cpp

---
+++ Classes-and-object/i-design/3/Donor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<stdio.h>
+#include<string>
+using namespace std;
+class Donor
+{
+public:
+    string name;
+    int age;
+    float height;
+    float weight;
+    string gender;
+    string bloodGroup;
+    
+    void display()
+    {
+        cout<<"Name :"<<name<<endl;
+        cout<<"Age :"<<age<<endl;
+        cout<<"height :"<<height<<endl;
+        cout<<"weight :"<<weight<<endl;
+        cout<<"Gender :"<<gender<<endl;
+        cout<<"Blood Group :"<<bloodGroup<<endl;
+        
+    }
+};
+
