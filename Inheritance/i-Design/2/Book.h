5c94f8ec1e228c423a346967f24ff7aef95035f0
hrsht0 <72308658+hrsht0@users.noreply.github.com>
2021-04-04 22:32:56 +0530

    Create Book.h

---
+++ Inheritance/i-Design/2/Book.h
@@ -0,0 +1,19 @@
+class Book{
+ protected:
+ string name;
+ string author;
+ int price;
+ string publication;
+ 
+ public:
+ Book(){}
+ 
+ Book(string name, string author, int price, string publication)
+ {
+ this->name=name;
+ this->author=author;
+ this->price=price;
+ this->publication=publication;
+ }
+ 
+};
